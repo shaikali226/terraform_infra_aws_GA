8753938df961259cddb1f4020d4b3887f404f0ba
shaikali <sharaphatali.shaik@gmail.com>
2026-02-17 12:55:33 +0000

    update backend

--- terraform/envs/backend.tf
+++ terraform/envs/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-tf-state-dev-17022026"
     key            = "github-project/dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
