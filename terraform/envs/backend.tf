dacd3a97c411c5080f68c09670e5c04d4c09d956
shaikali <sharaphatali.shaik@gmail.com>
2026-02-17 12:58:41 +0000

    path updated for backend

--- terraform/envs/backend.tf
+++ terraform/envs/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-tf-state-dev-17022026"
-    key            = "github-project/dev/terraform.tfstate"
+    key            = "dev/terraform.tfstate"
     region         = "ap-south-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
